b68f48a42f8349ea93c330720194f43452db8563
Siddharth <siddharthpn2003@gmail.com>
2026-01-24 22:49:35 +0530

    completed background color changer

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,30 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('black')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='  w-full h-full duration-200'
+    style={{backgroundColor: color}}
+    >
+      <div className='fixed flex flex-wrap gap-12 justify-center bottom-8 inset-x-0 px-2'>
+      <button onClick={()=>{setColor("Blue")}} className='flex px-6 rounded-xl outline-2 hover:scale-110 transition 1' style={{backgroundColor: "Blue"}}>Blue</button>
+      <button onClick={()=>{setColor("Red")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: "Red"}}>Red</button>
+      <button onClick={()=>{setColor("Green")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Green'}}>Green</button>
+      <button onClick={()=>{setColor("Pink")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Pink'}}>Pink</button>
+      <button onClick={()=>{setColor("Purple")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Purple'}}>Purple</button>
+      <button onClick={()=>{setColor("Orange")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Orange'}}>Orange</button>
+      <button onClick={()=>{setColor("Crimson")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Crimson'}}>Crimson</button>
+      <button onClick={()=>{setColor("Indigo")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Indigo'}}>Indigo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
