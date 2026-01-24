cf4590aa33ddd59bc88ad586782e3274a1ae067c
Siddharth <siddharthpn2003@gmail.com>
2026-01-25 00:06:50 +0530

    bg changer assets and app.jsx updated

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -11,7 +11,11 @@ function App() {
     <div className='  w-full h-full duration-200'
     style={{backgroundColor: color}}
     >
+      <div className='w-full h-screen flex items-center justify-center'>
+      <h2 className='text-2xl font-semibold px-6 h-10 rounded-3xl' style={{backgroundColor: 'wheat'}}>This is a backgroundColor Changer</h2>
+      </div>
       <div className='fixed flex flex-wrap gap-12 justify-center bottom-8 inset-x-0 px-2'>
+        
       <button onClick={()=>{setColor("Blue")}} className='flex px-6 rounded-xl outline-2 hover:scale-110 transition 1' style={{backgroundColor: "Blue"}}>Blue</button>
       <button onClick={()=>{setColor("Red")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: "Red"}}>Red</button>
       <button onClick={()=>{setColor("Green")}} className='flex px-6 rounded-xl outline-2 hover:scale-110' style={{backgroundColor: 'Green'}}>Green</button>
