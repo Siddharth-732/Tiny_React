f47207428c12b27553407ef48ea3f4abaa1055f8
Siddharth <siddharthpn2003@gmail.com>
2026-01-25 23:44:05 +0530

    project functional

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState , useCallback} from "react";
+import { useState, useCallback, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(8);
+  const [length, setlength] = useState(8);
   const [NumberAllowed, setNumberAllowed] = useState(false);
   const [CharAllowed, setCharAllowed] = useState(false);
   const [Password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz";
+
+    if (NumberAllowed) str += "1234567890";
+    if (CharAllowed) str += "@#$%^&*";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, NumberAllowed, CharAllowed,setPassword]);
+
+  useEffect(()=>{passwordGenerator()},[length, NumberAllowed, CharAllowed, passwordGenerator])
 
   return (
     <>
@@ -17,13 +33,45 @@ function App() {
             <div className="m-2 px-2 text-3xl">
               <h1 className="font-bold">Password Generator</h1>
             </div>
-            <div className="h-20 w-full bg-white"></div>
-            <div className="flex gap-2">
-              <input type="range" />
-              <input type="checkbox" />
-              <font>Numerals</font>
-              <input type="checkbox" />
-              <font>SpecialChar</font>
+            <div className="h-10 w-full bg-amber-50 flex justify-between">
+              <input
+                className="px-3"
+                type="text"
+                value={Password}
+                placeholder="Password"
+                readOnly
+              />
+              <button className="h-10 w-15 bg-amber-700 rounded-bl-lg text-amber-50">
+                Copy
+              </button>
+            </div>
+            <div className="flex gap-1">
+              <input
+                type="range"
+                min={6}
+                max={15}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => {
+                  setlength(e.target.value);
+                }}
+              />
+              <label>Length:{length}</label>
+              <input
+                type="checkbox"
+                defaultChecked={CharAllowed}
+                onChange={() => {
+                  setCharAllowed((prev) => !prev);
+                }}
+              />
+              <label>char</label>
+              <input 
+                type="checkbox"
+                defaultChecked={NumberAllowed}
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}/>
+              <label>Number</label>
             </div>
           </div>
         </div>
