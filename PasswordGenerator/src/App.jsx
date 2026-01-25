d1ae085169abfd5e2d13ccfcd48ed7cad15edbb7
Siddharth <siddharthpn2003@gmail.com>
2026-01-25 22:30:13 +0530

    frontend for the password gen

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(8);
+  const [NumberAllowed, setNumberAllowed] = useState(false);
+  const [CharAllowed, setCharAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
 
   return (
     <>
-      <h1>Password Generator</h1>
+      <div className=" w-full h-screen  bg-amber-100 flex justify-center items-center">
+        <div className="w-100 h-60 rounded-3xl bg-amber-300">
+          <div className="flex flex-col items-center gap-3">
+            <div className="m-2 px-2 text-3xl">
+              <h1 className="font-bold">Password Generator</h1>
+            </div>
+            <div className="h-20 w-full bg-white"></div>
+            <div className="flex gap-2">
+              <input type="range" />
+              <input type="checkbox" /><>Numerals</>
+              <input type="checkbox" /><>SpecialChar</>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
