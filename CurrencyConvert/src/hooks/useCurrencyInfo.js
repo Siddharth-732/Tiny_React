a68a3c9f93017be8c462167c31331b4d9721996f
Siddharth <siddharthpn2003@gmail.com>
2026-01-26 22:39:14 +0530

    Project functional

--- CurrencyConvert/src/hooks/useCurrencyInfo.js
+++ CurrencyConvert/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency) {
-  const [Data, setData] = useState({});
-  useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`,
-    )
-      .then((res) => res.json())
-      .then((res) => setData(res[currency]));
-  }, [currency]);
-  console.log(Data);
-  return Data;
-}
+export function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
 
-export default useCurrencyInfo;
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+      .catch((err) => console.error("Failed to fetch currency data", err));
+  }, [currency]);
+
+  return data;
+}
